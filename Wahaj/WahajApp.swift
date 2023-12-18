6a94a8a3d44c2d68a5d0559e47e5a5baa6b91471
Hasna Ahmad <hassna.ahmad98@gmail.com>
2023-12-18 10:37:26 +0300

    Initial Commit

---
+++ Wahaj/WahajApp.swift
@@ -0,0 +1,17 @@
+//
+//  WahajApp.swift
+//  Wahaj
+//
+//  Created by Hasna Ahmed on 05/06/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct WahajApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
