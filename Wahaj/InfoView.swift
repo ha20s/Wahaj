9e36946b5e3c6666d71582804fb5f2fa4affb288
Hasna Ahmad <hassna.ahmad98@gmail.com>
2023-12-18 12:32:54 +0300

    icons

---
+++ Wahaj/InfoView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoView.swift
+//  Wahaj
+//
+//  Created by Hasna Ahmed on 05/06/1445 AH.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoView()
+}
---
+++ Wahaj/StarsCategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  StarsCategoryView.swift
+//  Wahaj
+//
+//  Created by Hasna Ahmed on 05/06/1445 AH.
+//
+
+import SwiftUI
+
+struct StarsCategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StarsCategoryView()
+}
